3000b0c9c56e2e7dfc37fb98093440b1248b65d5
jia92ming <34341586+jia92ming@users.noreply.github.com>
2025-04-19 19:29:43 +0800

    Add files via upload

--- app/redirect_page/page.tsx
+++ app/redirect_page/page.tsx
@@ -1,7 +1,7 @@
 
 import { RedirectBtnAction} from '../shared-actions';
 
-export default async function redirect(p0: string){
+export default async function redirectShopPage(){
 
 return(<form action={RedirectBtnAction}>
     <h1 className="text-2xl font-bold mb-4"> No Items : Please Select Your Orders First! </h1>
